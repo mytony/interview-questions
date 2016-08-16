4820fcb0d91d142b1077220656700629e3a0719b
Meng-Yu Chung <tony0425@gmail.com>
2016-08-16 13:23:30 -0700

    Update xmlParser_tony.cpp

--- xmlParser_tony.cpp
+++ xmlParser_tony.cpp
@@ -13,7 +13,17 @@ public:
     }
 }
 
+// Assume we are given a tokenizer. It's method getNext() returns a tag once at a time
+// the tag has two fields: <name, tag>. 
+// E.g. <a> = <a, "open">
+// </b> = <b, "close">
+// the 'foo' in <c> foo </c> = <foo, "text">
 class XMLParser {
+    /*
+	LZ的方法是用stack来存parent, 遇到open和text建新node, 并把新node加到stack顶部node的child list里面，
+	如果是open就把新node压栈，遇到close就pop掉stack顶端的node 写完以后小哥的follow up是如果输入的token有问题，
+	比如close tag和open tag不匹配，如何做verification
+	*/
     XMLNode *root;
     stack<XMLNode*> stack;
 public:
