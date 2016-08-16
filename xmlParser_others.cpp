36ec61b54a98904eab3cfd6a90c208b3d56a3d64
Ya-Hsiu Yang <yanghilayco@gmail.com>
2016-08-16 11:58:45 -0700

    Create xmlParser_others.cpp

---
+++ xmlParser_others.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+class XMLMap {
+  public:
+    XMLMap() {
+      close = 1;
+    }
+    int close;
+    string content;
+    map<string, vector<XMLMap>> map;
+};
+
+void build(XMLMap& parent) {
+  XMLNode node = toknizer.Next();
+  string type = node.type;
+  string content = node.content;
+  if (type == "open") {
+    XMLMap child = XMLMap();
+    parent.map[content].push_back(child);
+  }
+  if (type == "inner") {
+    parent.content = content;
+  }
+  if (type == "close") {
+    parent.close--;
+  }
+}
+
+int main()
+{
+}
