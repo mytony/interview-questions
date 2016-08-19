371e734eb221532fecb0ff0f62ca08dc44c63dbe
Ya-Hsiu Yang <yanghilayco@gmail.com>
2016-08-19 00:01:58 -0700

    Update FindCommonManager.cpp

--- FindCommonManager.cpp
+++ FindCommonManager.cpp
@@ -5,13 +5,13 @@ public:
 };
 Employee* findCommonManager(Employee* manager, Employee* emp1, Employee* emp2) {
 	// if one of emp is manager then return manager
-	if (!manager || manager == emp1 || manager == emp2) 
+	if (!manager || manager->name == emp1->name || manager->name == emp2->name) 
 		return manager;
 	vector<Employee*> reporters;
-	for (Employee* emp:manager->directReportList) {
+	for (Employee* emp : manager->directReportList) {
 		Employee* rootEmp = findCommonManager(emp, emp1, emp2);
 		if (rootEmp) reporters.push_back(rootEmp);
 	}
-	if (reporters.size() >= 2) return manager;
+	if (reporters.size() == 2) return manager;
 	else return reporters[0];
 }
