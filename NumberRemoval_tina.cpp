787b6674c7a4ce2fe5eed710d9bf19b34ae30007
Ya-Hsiu Yang <yanghilayco@gmail.com>
2016-08-22 16:59:20 -0700

    Update NumberRemoval_tina.cpp

--- NumberRemoval_tina.cpp
+++ NumberRemoval_tina.cpp
@@ -2,9 +2,11 @@
 /* 给一个数组，元素是0-9的int，remove掉其中n个数，使剩下的数在原顺序的情况下最小。
 e.g. 7129，去掉两个数字，最小为12。 57294，去掉两个数字，剩下的最小为294。复杂度 O(length + n) 
 */
-int minValueByNumberRemoval(vector<int> nums) {
+int minValueByNumberRemoval(vector<int> nums, int n) {
 	// 把最小的放最前面，所以從最前面看步數(2)+1個數找最小放到最前面eg. 7129, 712內1最小所以把1前面都刪掉
-	int steps = 2;
+	// 刪除n個
+	if (n > nums.size()) return 0;
+	int steps = n;
 	int res = 0;
 	int min = INT_MAX;
 	int i = 0;
